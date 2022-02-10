060be74a74d5fa3b0f9429ade1d7f6da48fedab0
Ole-Kristian-Haaseth-Rudjord <s354564@oslomet.no>
2022-02-10 13:16:24 +0100

    Initial commit

---
+++ src/test/java/com/example/springbootforum1/SpringBootForum1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootforum1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootForum1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
